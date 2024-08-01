8b4ee7cc9dd85ed0aa56da3731adf67d2981f1e4
Marcos Vinícius Jacometti <marcosjacometti10@gmail.com>
2024-08-01 18:33:10 -0300

    test login

--- backend/controllers/users/loginController.js
+++ backend/controllers/users/loginController.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { dbConnection } = require("../../db/dbConnection");
-
 require('dotenv').config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+console.log('JWT_SECRET:', JWT_SECRET); // Verifica se JWT_SECRET está sendo carregado
+
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
+
+    console.log('Login attempt:', username);
 
     try {
         const findUserQuery = `
@@ -18,18 +21,23 @@ router.post('/login', async (req, res) => {
 
         dbConnection.query(findUserQuery, [username], async (err, results) => {
             if (err) {
-                console.error(err);
+                console.error('Database error:', err);
                 return res.status(500).send('Internal server error');
             }
 
             if (results.length === 0) {
+                console.log('User not found');
                 return res.status(404).send('User not found');
             }
 
             const user = results[0];
+            console.log('User found:', user);
+
             const passwordMatch = await bcrypt.compare(password, user.password);
+            console.log('Password match:', passwordMatch);
 
             if (!passwordMatch) {
+                console.log('Incorrect password');
                 return res.status(401).send('Incorrect password');
             }
 
@@ -49,10 +57,11 @@ router.post('/login', async (req, res) => {
                 { expiresIn: '2h' }
             );
 
+            console.log('Token generated:', token);
             res.status(200).json({ token });
         });
     } catch (err) {
-        console.error(err);
+        console.error('Catch block error:', err);
         res.status(500).send('Internal server error');
     }
 });
